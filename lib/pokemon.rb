6725325c5c0dcf388f6ab6cc20c75d8cbc26b7df
Allison _ <allimacq@gmail.com>
2021-01-23 23:23:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,10 +14,10 @@ class Pokemon
   def self.save(name, type, db)
     #new_pokemon = self.new(name: name, type: type, db: db)
     @db = db
-    @name = @db[:conn].execute("@SELECT * FROM pokemon WHERE name = ?, name")
+    @name = @db[:conn].execute("@SELECT * FROM pokemon WHERE name = ?", name)
     
     #@db[:conn].execute(sql, self.name, self.type)
-    @id = @db[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    #@id = @db[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
 end
